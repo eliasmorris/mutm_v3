8b934b7aa0f39fd96980f6fd1b7d3b7a1e2b4470
Elias morris <30745358+eliasmorris@users.noreply.github.com>
2024-09-11 12:40:50 +0300

    update

--- business_controller/instituteList.php
+++ business_controller/instituteList.php
@@ -19,34 +19,7 @@
           echo '<td class="font-weight-bold">'.$value['email'].'</td>';
           echo '<td>'.$value['phonenumber'].'</td>';
           echo '<td>'.$value['shehia'].'</td>';
-          // if ($value['inststatus'] == 'inactive') {
-          //   echo '<td class="text-danger">'.strtoupper($value['inststatus']).'</td>';
-          // }else{
-          //   echo '<td>'.strtoupper($value['inststatus']).'</td>';
-          // }
-            
-            // if ($_SESSION['urole'] == 'Msimamizi mkuu') {
-            //   if ($value['inststatus'] == 'active') {
-            //     echo '<td class="text-right">';
-            //       echo '<div class="btn-group">';
-            //         echo '<a data-conf0="'.$value['instituteid'].'" data-conf2="'.$value['instcode'].'" data-conf3="'.$value['instname'].'" href="#addPOS" class="btn btn-xs btn-warning open-addPOS" title="Bonyeza Kusajili POS katika Taasisi"><i class="fas fa-tablet-alt"></i>&nbsp;POS</a>';
-
-            //         include('../Controller/account_controller/insertAccount.php'); //query to insert account no
-            //         echo '<a data-conf0="'.$value['instituteid'].'" data-conf2="'.$value['instcode'].'" data-conf3="'.$value['instname'].'" data-conf4="'.$insertAccount.'" href="#addAccountNo" class="btn btn-xs btn-success open-addAccountNo" title="Bonyeza Kusajili Namba za Akaunti ya Kupokea Malipo ya Taasisi"><i class="fas fa-plus"></i>&nbsp;Akaunti</a>';
-
-            //         echo '<a data-id="'.$value['instituteid'].'" data-conf2="'.$value['instcode'].'" data-conf3="'.$value['instname'].'" data-conf4="'.$value['insttype'].'" data-conf5="'.$value['regname'].'" data-conf6="'.$value['regid'].'" data-conf7="'.$value['intergratingcode'].'" href="#editInstitutionf" class="btn btn-xs btn-info open-editInstitutionf" title="Bonyeza kubadili taariza za taasisi"><i class="fas fa-pencil-alt"></i></a>';
-                      ?>
-            <!-- //           <a class="btn btn-xs btn-danger" onClick="deleteInstitution('<?php echo $value['instituteid']; ?>', '<?php echo $value['instname']; ?>')" title="Bonyeza kufuta taasisi"><i class="fas fa-trash"></i></a> -->
-            <!-- //       </div> -->
-                   <?php
-            //     echo'</td>';
-            //   }else{
-            //     echo '<td class = "text-right">&nbsp;</td>';
-            //   }
-              
-            // }else{
-            //   echo '<td class="text-right">&nbsp;</td>';
-            // }
+          
             
           echo '</tr>';
 
