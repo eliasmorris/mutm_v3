d8e7d2902a79c63de4fd368a544e7e2d12c9ef28
Elias morris <30745358+eliasmorris@users.noreply.github.com>
2024-09-20 14:19:03 +0300

    checking query

--- business_controller/index.php
+++ business_controller/index.php
@@ -83,7 +83,11 @@
                 <?php
 
                   // $json = file_get_contents($pubIP.'selectInstitutionInfo'); //receive json from url
-                  $json = file_get_contents($publicIPConnent.'mutm/api/getAllBusiness?pageNum=1&pageSize=20');
+                  if($json = file_get_contents($publicIPConnent.'mutm/api/getAllBusiness?pageNum=1&pageSize=20')){
+                    echo 'hello';
+                  }else{
+                    echo 'error';
+                  }
                   $arr = json_decode($json, true); //covert json data into array format
 
                   include('biasharaList.php');
