ffd57b664ca0ca900a7878b27785612d6585fc0a
Elias morris <30745358+eliasmorris@users.noreply.github.com>
2024-09-20 14:35:32 +0300

    undo the change

--- business_controller/index.php
+++ business_controller/index.php
@@ -83,11 +83,7 @@
                 <?php
 
                   // $json = file_get_contents($pubIP.'selectInstitutionInfo'); //receive json from url
-                  if($json = file_get_contents($publicIPConnent.'mutm/api/getAllBusiness?pageNum=1&pageSize=20')){
-                    echo 'hello';
-                  }else{
-                    echo 'error';
-                  }
+                  $json = file_get_contents($publicIPConnent.'mutm/api/getAllBusiness?pageNum=1&pageSize=20');
                   $arr = json_decode($json, true); //covert json data into array format
 
                   include('biasharaList.php');
