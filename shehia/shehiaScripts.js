1aa128021fecaee7448d656ae1e67b9c919f8b1b
Elias morris <30745358+eliasmorris@users.noreply.github.com>
2024-12-11 09:59:10 +0300

    some change

--- shehia/shehiaScripts.js
+++ shehia/shehiaScripts.js
@@ -9,7 +9,7 @@ form.addEventListener('submit', function (e) {
   var publicIP = document.getElementById('publicIPa').value;
   var localIPa = document.getElementById('localIPa').value;
 
-  fetch(localIPa + "mutm/api/insertShehia", {
+  fetch(publicIP + "mutm/api/insertShehia", {
     method: 'POST',
     //mode: 'no-cors',  // This disables CORS
 
