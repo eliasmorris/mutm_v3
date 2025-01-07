7f91de9e27748ac6b55b31c4c104f9abae77e364
Elias morris <30745358+eliasmorris@users.noreply.github.com>
2025-01-07 09:38:00 +0300

    some change

--- malipo/gatemailphone.php
+++ malipo/gatemailphone.php
@@ -1,11 +1,11 @@
 <?php
 //session_start();
 include('../Controller/connect.php');
-
+include("../Controller/configuration.php"); //configuration file
 $fullname = $_POST['fullname'];
 
 $json = file_get_contents($pubIP1 . 'mutm/api/getBusinessByName/'.$fullname); //receive json from url
-$arr = (array)json_decode($json, TRUE); //covert json data into array format
+$arr = json_decode($json, TRUE); //covert json data into array format
 $value['email'] = $arr['email'];
 $value['phonenumber'] = $arr['phonenumber'];
 ?>
