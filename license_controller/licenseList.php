68e9bb26bef467a61ff6537b1e966e39043d6434
Elias morris <30745358+eliasmorris@users.noreply.github.com>
2025-01-08 15:58:14 +0300

    some change in license controler

--- license_controller/licenseList.php
+++ license_controller/licenseList.php
@@ -10,67 +10,69 @@
       <th class="text-center">Kiwango</th>
       <th class="text-center">Hali</th>
       <th class="text-center">Uhakiki</th>
-      <th class="text-center">Kitendo</th>  
+      <th class="text-center">Kitendo</th>
     </tr>
   </thead>
   <tbody>
     <?php
     $num = 1;
     foreach ($arr as $key => $value) {
-      echo '<tr>';
-      echo '<td>' . $num . '</td>';
-      echo '<td>' . $value['lnumber'] . '</td>';
-      echo '<td>' . $value['licensetype'] . '</td>';
-      echo '<td>' . $value['category'] . '</td>';
-      echo '<td>' . $value['bname'] . '</td>';
-      echo '<td>' . $value['shname'] . '</td>';
-      echo '<td>' . $value['amount'] . '</td>';
-      if ($value['cstatus'] == 'PAID') {
-        echo '<td class = "text-right">&nbsp;<button class="btn btn-sm btn-success" >PAID</button></td>';
-      } else {
-        echo '<td class = "text-right">&nbsp;<button class="btn btn-sm btn-warning">CREATED</button></td>';
-      }
+      if ($value['isapproved'] == false && $value['cstatus'] == 'PAID') {
+        echo '<tr>';
+        echo '<td>' . $num . '</td>';
+        echo '<td>' . $value['lnumber'] . '</td>';
+        echo '<td>' . $value['licensetype'] . '</td>';
+        echo '<td>' . $value['category'] . '</td>';
+        echo '<td>' . $value['bname'] . '</td>';
+        echo '<td>' . $value['shname'] . '</td>';
+        echo '<td>' . $value['amount'] . '</td>';
+        if ($value['cstatus'] == 'PAID') {
+          echo '<td class = "text-right">&nbsp;<button class="btn btn-sm btn-success" >PAID</button></td>';
+        } else {
+          echo '<td class = "text-right">&nbsp;<button class="btn btn-sm btn-warning">CREATED</button></td>';
+        }
 
-      if ($value['isapproved'] == true && $value['cstatus'] == 'PAID') {
+        if ($value['isapproved'] == true && $value['cstatus'] == 'PAID') {
 
-        echo '<td class = "text-right">&nbsp;<button class="btn btn-sm btn-success" >Approved</button></td>';
-      } else{
+          echo '<td class = "text-right">&nbsp;<button class="btn btn-sm btn-success" >Approved</button></td>';
+        } else {
 
-        echo '<td class = "text-right">&nbsp;<button class="btn btn-sm btn-warning">Not&nbsp;Approved</button></td>';
-      }
+          echo '<td class = "text-right">&nbsp;<button class="btn btn-sm btn-warning">Not&nbsp;Approved</button></td>';
+        }
 
-      if ($value['isapproved'] != true ) {
-        echo '<td class="text-right">';
-        echo '<div class="btn-group">';
-        if ($_SESSION['urole'] == 'Mwenyekiti bodi') {
-          if ($value['cstatus'] == 'PAID') {
+        if ($value['isapproved'] != true) {
+          echo '<td class="text-right">';
+          echo '<div class="btn-group">';
+          if ($_SESSION['urole'] == 'Mwenyekiti bodi') {
+            if ($value['cstatus'] == 'PAID') {
+    ?>
+              <a class="btn btn-sm btn-primary" onclick="approveLicenseinfo('<?php echo $value['lid']; ?>', '<?php echo $value['lnumber']; ?>')" title="Bonyeza kuhakiki Leseni">Approve</a>
+              </div>
+            <?php
+            }
+            // echo '<a data-id="' . $value['lid'] . '" data-conf2="' . $value['lnumber'] . '" data-conf3="' . $value['licensetype'] . '" data-conf4="' . $value['bname'] . '" data-conf5="' . $value['amount'] . '" data-conf6="'.$value['businessid'].'" data-conf7="'.$value['category'].'" href="#editLicense" class="btn btn-xs btn-info open-editLicenseinfo" title="Bonyeza kubadili taariza za Leseni"><i class="fas fa-pencil-alt"></i></a>';
             ?>
-            <a class="btn btn-sm btn-primary" onclick="approveLicenseinfo('<?php echo $value['lid']; ?>', '<?php echo $value['lnumber']; ?>')" title="Bonyeza kuhakiki Leseni">Approve</a>
-          </div>
+
           <?php
           }
-          // echo '<a data-id="' . $value['lid'] . '" data-conf2="' . $value['lnumber'] . '" data-conf3="' . $value['licensetype'] . '" data-conf4="' . $value['bname'] . '" data-conf5="' . $value['amount'] . '" data-conf6="'.$value['businessid'].'" data-conf7="'.$value['category'].'" href="#editLicense" class="btn btn-xs btn-info open-editLicenseinfo" title="Bonyeza kubadili taariza za Leseni"><i class="fas fa-pencil-alt"></i></a>';
-    ?>
-          
-    <?php
-        }
-        echo '</td>';
-      } else {
-        echo '<td class="text-right">';
-        echo '<div class="btn-group">';
-        if ($_SESSION['urole'] == 'Afisa mapato') {
-
-          echo '<a href="printLicense.php?licenseId=' . $value['lid'] . '&businessid=' . $value['businessid'] . '&businesscategory=' . $value['category'] . '" target="_blank" class="btn btn-primary btn-xs" title="Bonyeza kuprint leseni"><i class="fas fa-print"></i>Print</a>';
-          //echo '<td class="text-right">&nbsp;</td>';
-          ?>
-          </div>
-          <?php
           echo '</td>';
-        }
-      }
-      echo '</tr>';
+        } else {
+          echo '<td class="text-right">';
+          echo '<div class="btn-group">';
+          if ($_SESSION['urole'] == 'Afisa mapato') {
 
-      $num++;
+            echo '<a href="printLicense.php?licenseId=' . $value['lid'] . '&businessid=' . $value['businessid'] . '&businesscategory=' . $value['category'] . '" target="_blank" class="btn btn-primary btn-xs" title="Bonyeza kuprint leseni"><i class="fas fa-print"></i>Print</a>';
+            //echo '<td class="text-right">&nbsp;</td>';
+          ?>
+            </div>
+    <?php
+            echo '</td>';
+          }
+        }
+        echo '</tr>';
+
+        $num++;
+      }
     }
     ?>
   </tbody>
