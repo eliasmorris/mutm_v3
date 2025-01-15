804917130c1d5dc91f5d49db27e3b0cfa40d5f4f
Elias morris <30745358+eliasmorris@users.noreply.github.com>
2025-01-15 14:12:25 +0300

    remove amount from printed license

--- license_controller/printLicense.php
+++ license_controller/printLicense.php
@@ -192,7 +192,8 @@
                         </h2>
                         <br>
                         <br>
-                        <h2 style="text-align: center;"> <b>FEE PAID TSH <?php echo number_format($arr['amount'], 2) . '/='; ?></b></h2>
+                        <!-- $arr['amount'], 2 -->
+                        <h2 style="text-align: center;"> <b>FEE PAID TSH <?php echo number_format('3,400,000') . '/='; ?></b></h2>
                         <br>
                         <br>
                     </div>
